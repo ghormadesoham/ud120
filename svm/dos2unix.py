1f05e049cc848f4665b4119a0de2b379c02b0513
SohamGhormade <soham.ghormade@gmail.com>
2017-08-20 16:27:48 -0400

    US 1:First commit
    Update code to follow python 3 guidelines
    -use pickle instead cpickle for serialization/deserialization
    -use brackets after print

---
+++ svm/dos2unix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""\
+convert dos linefeeds (crlf) to unix (lf)
+usage: dos2unix.py <input> <output>
+"""
+#words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"
+original = "../tools/word_data.pkl"
+destination = "../tools/word_data_unix.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+  content = infile.read()
+with open(destination, 'wb') as output:
+  for line in content.splitlines():
+    outsize += len(line) + 1
+    output.write(line + str.encode('\n'))
+
+print("Done. Saved %s bytes." % (len(content)-outsize))

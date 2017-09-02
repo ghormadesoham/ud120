219e38db1edb57f810472d0fdb587e93b9588b08
SohamGhormade <soham.ghormade@gmail.com>
2017-09-02 16:48:45 -0400

    US 3:add a PDF doc with lecture notes on information gain and entropy.
    Add images on decision tree classifier.

--- decision_tree/class/classifyDT.py
+++ decision_tree/class/classifyDT.py
@@ -1,6 +1,11 @@
 def classify(features_train, labels_train):
 ### your code goes here--should return a trained decision tree classifer
  from sklearn import tree
- clf = tree.DecisionTreeClassifier()
+ clf = tree.DecisionTreeClassifier(min_samples_split = 50)
  clf = clf.fit(features_train, labels_train)   
  return clf
+#Notes
+#min_samples_split parameter
+#The minimum number of samples required to split an internal node
+#increase the default value of 2 to 50 so see the jagged boundary of the decision surface become uniform
+
